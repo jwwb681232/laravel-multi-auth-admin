bfac267587a7a2974310d005f39690674e59d928
wangxiao <wx497657341@qq.com>
2017-07-19 14:58:12 +0800

    修复entrust在多表认证的bug

--- database/migrations/2017_06_29_024954_entrust_setup_tables.php
+++ database/migrations/2017_06_29_024954_entrust_setup_tables.php
@@ -25,7 +25,7 @@ class EntrustSetupTables extends Migration
             $table->integer('user_id')->unsigned();
             $table->integer('role_id')->unsigned();
 
-            $table->foreign('user_id')->references('id')->on('users')
+            $table->foreign('user_id')->references('id')->on('admins')
                 ->onUpdate('cascade')->onDelete('cascade');
             $table->foreign('role_id')->references('id')->on('roles')
                 ->onUpdate('cascade')->onDelete('cascade');
